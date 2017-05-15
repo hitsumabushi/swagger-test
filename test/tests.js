5b73af8620ef96d441e9d577c85337cc2bf06d10
hitsumabushi <hitsumabushi.org@gmail.com>
2017-05-16 01:01:22 +0900

    WIP! format json

--- test/tests.js
+++ test/tests.js
@@ -8,71 +8,71 @@ var assert = require('chai').assert;
 
 describe('test generation with inference', function () {
 
-  var testDir = __dirname;
-  var buffer  = fs.readFileSync(testDir + '/swagger.json');
-  var spec    = JSON.parse(buffer);
+    var testDir = __dirname;
+    var buffer = fs.readFileSync(testDir + '/swagger.json');
+    var spec = JSON.parse(buffer);
 
-  var xamples = swaggerTest.parse(spec, { inferXamples: true });
+    var xamples = swaggerTest.parse(spec, {inferXamples: true});
 
-  it('should contain three test cases', function () {
-    assert.equal(xamples.length, 3);
-  });
+    it('should contain three test cases', function () {
+        assert.equal(xamples.length, 3);
+    });
 
-  it('should first test GET /pets', function () {
-      assert.deepEqual(xamples[0], {
-          "description": "get /pets",
-          "request": {
-              "method": "get",
-              "uri": "http://localhost/v1/pets"
-          },
-          "responses": {
-              "200": {
-                "headers": {
-                    "content-type": "application/json"
-                }
-              }
-          }
-      });
-  });
-
-  it ('should next test GET /pets/fido4', function () {
-      assert.deepEqual(xamples[1], {
-          "description": "get /pets/{id}: returns 200 for fido4",
-          "request": {
-              "params": {
-                  "id": "fido4"
-              },
-              "method": "get",
-              "uri": "http://localhost/v1/pets/fido4"
-          },
-          "responses": {
-              "200": {
-                "headers": {
-                    "content-type": "application/json"
-                }
-              }
-          }
-      });
-  });
-
-  it ('should next test GET /pets/fido7', function () {
-      assert.deepEqual(xamples[2], {
-        "description": "get /pets/{id}",
-        "request": {
-            "params": {
-                "id": "fido7"
+    it('should first test GET /pets', function () {
+        assert.deepEqual(xamples[0], {
+            "description": "get /pets",
+            "request": {
+                "method": "get",
+                "uri": "http://localhost/v1/pets"
             },
-            "method": "get",
-            "uri": "http://localhost/v1/pets/fido7"
-        },
-        "responses": {
-            "200": {
-              "headers": {
-                  "content-type": "application/json"
-              }
+            "responses": {
+                "200": {
+                    "headers": {
+                        "content-type": "application/json"
+                    }
+                }
             }
-        }
-      });
-  });
+        });
+    });
+
+    it('should next test GET /pets/fido4', function () {
+        assert.deepEqual(xamples[1], {
+            "description": "get /pets/{id}: returns 200 for fido4",
+            "request": {
+                "params": {
+                    "id": "fido4"
+                },
+                "method": "get",
+                "uri": "http://localhost/v1/pets/fido4"
+            },
+            "responses": {
+                "200": {
+                    "headers": {
+                        "content-type": "application/json"
+                    }
+                }
+            }
+        });
+    });
+
+    it('should next test GET /pets/fido7', function () {
+        assert.deepEqual(xamples[2], {
+            "description": "get /pets/{id}",
+            "request": {
+                "params": {
+                    "id": "fido7"
+                },
+                "method": "get",
+                "uri": "http://localhost/v1/pets/fido7"
+            },
+            "responses": {
+                "200": {
+                    "headers": {
+                        "content-type": "application/json"
+                    }
+                }
+            }
+        });
+    });
 
 });
